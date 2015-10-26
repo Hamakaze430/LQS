eb1f944ca35a64c4f9c882816740bac186b8178e
ryssuke <admin@kirachiyan.local>
2015-10-26 22:53:30 +0800

    added files.

--- Client/src/main/java/po/UserPO.java
+++ Client/src/main/java/po/UserPO.java
@@ -1,6 +1,18 @@
 package po;
 
 import java.io.Serializable;
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+
+public class UserPO implements Serializable {
+    int id;
+    String name;
+    String password;
+    String job;
+
+>>>>>>> Stashed changes
 
 public class UserPO implements Serializable{
 	int id;
