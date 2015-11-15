8b1fd2c5c18ecc64860349fbe73a7a0c00e0d3f4
lx14 <260270238@qq.com>
2015-11-15 19:19:35 +0800

    change

--- Client/src/main/java/businessLogic/Receiptsbl/Receiptsbl.java
+++ Client/src/main/java/businessLogic/Receiptsbl/Receiptsbl.java
@@ -1,9 +1,12 @@
 package businessLogic.Receiptsbl;
 
 import businessLogicService.ReceiptsblService.ReceiptsblService;
+import dataService.dataFactoryService.DataFactoryService;
+import init.RMIHelper;
 
 public class Receiptsbl implements ReceiptsblService {
 	
+<<<<<<< HEAD
 	
 	public String showVouchers() {
 		return null;
@@ -13,4 +16,15 @@ public class Receiptsbl implements ReceiptsblService {
 		return 0;
 	}
 
+=======
+	private DataFactoryService df;
+	public Receiptsbl(){
+		df = RMIHelper.getDataFactory();
+	}
+	public String getLoadingId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+>>>>>>> origin/master
 }
