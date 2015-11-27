5e45caaeb5ff63c2803006ab061c2dc3f5106fbc
ryssuke <ryysuke@kirachiyan.local>
2015-11-27 15:18:52 +0800

    uum.

--- Server/src/main/java/data/DataFactory/DataFactory.java
+++ Server/src/main/java/data/DataFactory/DataFactory.java
@@ -2,7 +2,7 @@ package data.DataFactory;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-
+import po.*;
 import data.LogisticsInfoSearchdata.LogisticsDataServiceTextFileImpl;
 import dataService.DataFactoryService.DataFactoryService;
 import dataService.LogisticsInfoSearchdataService.LogisticsDataService;
@@ -20,4 +20,53 @@ public class DataFactory extends UnicastRemoteObject implements DataFactoryServi
 		return data;
 	}
 
+	public UserPO getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BillManagementPO getBM() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SalesPO getSales() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CostBenefitPO getCB() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getName(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CarPO getCar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DriverPO getDriver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CommodityPO getCommodity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BankAccountPO getBankAccount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BillManagementPO getBM(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Server/src/main/java/dataService/DataFactoryService/DataFactoryService.java
+++ Server/src/main/java/dataService/DataFactoryService/DataFactoryService.java
@@ -4,8 +4,33 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import dataService.LogisticsInfoSearchdataService.LogisticsDataService;
+import po.*;
 
 public interface DataFactoryService extends Remote {
 	
 	LogisticsDataService getLogisticsDataService() throws RemoteException;
+	
+	public UserPO getUser();
+	
+	public BillManagementPO getBM(String date);
+	
+	public SalesPO getSales();
+	
+	public CostBenefitPO getCB();
+	
+	public String getName();
+	
+	public CarPO getCar();
+	
+	public DriverPO getDriver();
+	
+	public CommodityPO getCommodity();
+	
+	public BankAccountPO getBankAccount();
+	
+	//public void saveBMPO(BillManagementPO bm);
+	
+	public ReceiptPO getReceiptbyDate(String date);
+	
+	public void writeLog(String log);
 }
